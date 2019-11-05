a3a09c69e3c3a835e27e7a155194b1779677b21f
xww <xiongwenwu1997@126.com>
2019-11-05 22:48:11 +0800

    Update problem_detail.py

--- problem_detail.py
+++ problem_detail.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 import copy
+
+
 class ProblemDetail(object):
     def __init__(self):
         self.id = 0
@@ -49,40 +51,42 @@ def com_cmp(x, y):
 
 class LeetCodeStatics(object):
     def __init__(self):
-        self.probles_detail = []
+        self.problems_detail = []
         self.coms_detail = {}
         self.name_map = {'字节跳动': '今日头条'}
 
     def file2probles_detail(self):
         f = open('aa.txt', 'r')
         for line in f.readlines():
-
             temp = line[:-1].split(',')
-            if int(temp[0]) == 50:
+            if int(temp[0]) == 50:#第50题题目本身有逗号
                 temp[1] += temp[2]
                 temp.remove(temp[2])
 
             problem_detail = ProblemDetail()
-            problem_detail.id = temp[0]
+            problem_detail.id = int(temp[0])
             problem_detail.title = temp[1]
             problem_detail.url = temp[2]
             problem_detail.level = temp[3]
 
             for i in range(4, len(temp)):
+
                 com_name = temp[i].split(':')[0]
                 real_name = com_name
                 if com_name in self.name_map:
                     real_name = self.name_map[com_name]
                 com_counts = temp[i].split(':')[1]
+
                 problem_detail.total_counts += int(com_counts)
+
                 if com_name in problem_detail.coms:
                     problem_detail.coms[real_name] = problem_detail.coms[real_name] + int(com_counts)
                 else:
                     problem_detail.coms[real_name] = int(com_counts)
-            self.probles_detail.append(problem_detail)
+            self.problems_detail.append(problem_detail)
 
     def gen_coms_detail(self):
-        for problem_detail in self.probles_detail:
+        for problem_detail in self.problems_detail:
             for com_name in problem_detail.coms.keys():
                 temp_problem = copy.deepcopy(problem_detail)
                 temp_problem.owner_com_name = com_name
@@ -100,16 +104,16 @@ class LeetCodeStatics(object):
 
 if __name__ == '__main__':
     leetccodestatics = LeetCodeStatics()
-    leetccodestatics.file2probles_detail()
+    leetccodestatics.file2problems_detail()
 
     # coms_name = set()
-    # for problem_detail in leetccodestatics.probles_detail:
+    # for problem_detail in leetccodestatics.problems_detail:
     #     for com_name in problem_detail.coms.keys():
     #         coms_name.add(com_name)
     # for name in list(coms_name):
     #     print name
 
-    # problems = leetccodestatics.probles_detail
+    # problems = leetccodestatics.problems_detail
     # sorted_problems = sorted(problems, cmp=problem_cmp)
     # for p in sorted_problems:
     #     print p.total_counts, p.url
